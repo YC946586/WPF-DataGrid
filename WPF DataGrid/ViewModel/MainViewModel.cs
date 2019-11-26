5605c2778e2d8b1e21fba883162ff4e81398b5e6
Hazy <Hazy@DESKTOP-HPU66I4>
2019-11-26 10:53:06 +0800

    add Ischeck

--- WPF DataGrid/ViewModel/MainViewModel.cs
+++ WPF DataGrid/ViewModel/MainViewModel.cs
@@ -1,5 +1,8 @@
 using GalaSoft.MvvmLight;
 using System.Collections.ObjectModel;
+using System.Linq;
+using GalaSoft.MvvmLight.Command;
+using System;
 
 namespace WPF_DataGrid.ViewModel
 {
@@ -17,21 +20,193 @@ namespace WPF_DataGrid.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+
+        private bool _checkAll;
+
+        /// <summary>
+        /// 全选
+        /// </summary>
+        public bool CheckAll
+        {
+            get { return _checkAll; }
+            set
+            {
+                _checkAll = value;
+                if (GridModelList.Count != 0)
+                {
+                    GridModelList.ToList().ForEach((ary) => ary.IsCheck = CheckAll);
+                }
+                RaisePropertyChanged();
+            }
+        }
+
+        private string searchText = string.Empty;
+        private RelayCommand _QueryCommand;
+
+        private RelayCommand _AllDelCommand;
+        private RelayCommand<MainModel> _DelCommand;
         private ObservableCollection<MainModel> _gridModelList = new ObservableCollection<MainModel>();
+
+
+
+
+        /// <summary>
+        /// 搜索内容
+        /// </summary>
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                searchText = value;
+                RaisePropertyChanged();
+            }
+        }
+
         /// <summary>
         /// 表单数据
         /// </summary>
         public ObservableCollection<MainModel> GridModelList
         {
             get { return _gridModelList; }
-            set { _gridModelList = value; RaisePropertyChanged(); }
+            set
+            {
+                _gridModelList = value;
+                RaisePropertyChanged();
+            }
         }
 
+        /// <summary>
+        /// 查询
+        /// </summary>
+        public RelayCommand QueryCommand
+        {
+            get
+            {
+                if (_QueryCommand == null)
+                {
+                    _QueryCommand = new RelayCommand(() => Query());
+                }
+                return _QueryCommand;
+            }
+            set { _QueryCommand = value; }
+        }
+
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        public RelayCommand AllDelCommand
+        {
+            get
+            {
+                if (_AllDelCommand == null)
+                {
+                    _AllDelCommand = new RelayCommand(() => AllDel());
+                }
+                return _AllDelCommand;
+            }
+            set { _AllDelCommand = value; }
+        }
+        /// <summary>
+        /// 删除
+        /// </summary>
+        public RelayCommand<MainModel> DelCommand
+        {
+            get
+            {
+                if (_DelCommand == null)
+                {
+                    _DelCommand = new RelayCommand<MainModel>(t => Del(t));
+                }
+                return _DelCommand;
+            }
+            set { _DelCommand = value; }
+        }
+
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        private void Query()
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(SearchText))
+                {
+                    var queryList = GridModelList.Where(s => s.FunName.Contains(SearchText)).ToList();
+                    if (queryList.Any())
+                    {
+                        GridModelList.Clear();
+                        queryList.ForEach((ary=> GridModelList.Add(ary)));
+                    }
+                    
+                }
+                else
+                {
+                    InitUi();
+                }
+                    
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+     
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="model"></param>
+        private void Del(MainModel model)
+        {
+            try
+            {
+                GridModelList.Remove(model);
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        private void AllDel()
+        {
+            try
+            {
+                if (GridModelList.Count != 0)
+                {
+                
+                    //判断界面绑定数据源 是否有勾选的数据 
+                    var notDelModel = GridModelList.Where(s => !s.IsCheck).ToList();
+                    GridModelList.Clear();
+                    if (notDelModel.Any())
+                    {  
+                        notDelModel.ForEach((ary => GridModelList.Add(ary)));
+                    }  
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            InitUi();
+        }
+
+        private void InitUi()
+        {
+            GridModelList = new ObservableCollection<MainModel>();
             for (int i = 0; i < 6; i++)
             {
                 MainModel model = new MainModel()
@@ -50,6 +225,21 @@ namespace WPF_DataGrid.ViewModel
     /// </summary>
     public  class MainModel : ViewModelBase
     {
+
+        private bool _isCheck;
+        /// <summary>
+        /// 选择
+        /// </summary>
+        public bool IsCheck
+        {
+            get { return _isCheck; }
+            set
+            {
+                _isCheck = value;
+                RaisePropertyChanged();
+            }
+        }
+
         private string _funName;
         /// <summary>
         /// 节点名称
