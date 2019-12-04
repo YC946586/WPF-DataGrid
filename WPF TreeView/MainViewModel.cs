9982e115853138a18e96970b61e17c06c2f12102
Hazy <Hazy@DESKTOP-HPU66I4>
2019-12-04 19:31:44 +0800

    Tree

---
+++ WPF TreeView/MainViewModel.cs
@@ -0,0 +1,153 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight.Command;
+
+namespace WPF_TreeView
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<MenuGroupModel> _MenuGroupModels = new ObservableCollection<MenuGroupModel>();
+
+        /// <summary>
+        /// Tree功能节点
+        /// </summary>
+        public ObservableCollection<MenuGroupModel> MenuGroupModels
+        {
+            get { return _MenuGroupModels; }
+            set
+            {
+                _MenuGroupModels = value;
+                RaisePropertyChanged();
+            }
+        }
+        private  object _selectMenuGroup;
+      /// <summary>
+      /// 选择的数据
+      /// </summary>
+        public  object SelectMenuGroup
+        {
+            get { return _selectMenuGroup; }
+            set
+            {
+                _selectMenuGroup = value;
+                RaisePropertyChanged();
+            }  
+        }
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        public MainViewModel()
+        {
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    MenuGroupModel loader = new MenuGroupModel
+                    {
+                        ID = i,
+                        MenuName = "洋葱Biu" + i,
+                    };
+                    BindingChildNode(loader);
+                    MenuGroupModels.Add(loader);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 绑定子节点  递归    通过父级ID 找到下层节点 
+        /// </summary>
+        /// <param name="treeViewItem"></param>
+        private static void BindingChildNode(MenuGroupModel treeViewItem)
+        {
+            try
+            {
+                for (int i =1; i < 3; i++)
+                {
+                    MenuGroupModel groupModel = new MenuGroupModel
+                    {
+                        ID = i + 10,
+                        ParentId = treeViewItem.ID,
+                        MenuName = "洋葱Biu" + i + 10,
+                        CurModel= "WPF_TreeView.SelectView.UserControl"+i
+                    };
+                    treeViewItem.Nodes.Add(groupModel);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+        private RelayCommand<MenuGroupModel> _selectCommand;
+
+        /// <summary>
+        /// 选中
+        /// </summary>
+        public RelayCommand<MenuGroupModel> SelectComamnd
+        {
+            get
+            {
+                if (_selectCommand == null)
+                {
+                    _selectCommand = new RelayCommand<MenuGroupModel>(Select);
+                }
+                return _selectCommand;
+            }
+            set { _selectCommand = value; }
+        }
+
+        /// <summary>
+        /// 选中
+        /// </summary>
+        /// <param name="model"></param>
+        private void Select(MenuGroupModel model)
+        {
+            try
+            {
+                if (model.CurModel!=null)
+                {
+                    SelectMenuGroup = GetUserControl(model.CurModel.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// 反射UserControl
+        /// </summary>
+        /// <returns></returns>
+        private  UserControl GetUserControl(string userctolAction)
+        {
+            try
+            {
+                UserControl uc = (UserControl)Assembly.Load(Assembly.GetExecutingAssembly().FullName).CreateInstance(userctolAction);
+                
+                return uc;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw;
+            }
+        }
+    }
+}
---
+++ WPF TreeView/MenuGroupModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace WPF_TreeView
+{
+    /// <summary>
+    /// 树形菜单  商品编码维护
+    /// </summary>
+    [Serializable]
+    public class MenuGroupModel : ViewModelBase
+    {
+        public MenuGroupModel()
+        {
+            Nodes = new ObservableCollection<MenuGroupModel>();
+        }
+
+        private bool _IsChecked = false;
+        private bool _IsSelected = false;
+        private string _menuName;
+        /// <summary>
+        /// 主键序号
+        /// </summary>
+        public int ID
+        {
+            get; set;
+        }
+
+
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string MenuName { get { return _menuName; } set { _menuName = value; RaisePropertyChanged(); } }
+        /// <summary>
+        /// 菜单代码
+        /// </summary>
+        public string MenuCode { get; set; }
+
+        /// <summary>
+        /// 父级ID
+        /// </summary>
+        public int ParentId
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 当前的实体
+        /// </summary>
+        public object CurModel
+        {
+            get; set;
+        }
+        /// <summary>
+        /// 权限值
+        /// </summary>
+        public int? AuthValue { get; set; }
+
+        /// <summary>
+        /// 是否选择
+        /// </summary>
+        public bool IsChecked { get { return _IsChecked; } set { _IsChecked = value; RaisePropertyChanged(); } }
+
+        /// <summary>
+        /// 是否选中
+        /// </summary>
+        public bool IsSelected { get { return _IsSelected; } set { _IsSelected = value; RaisePropertyChanged(); } }
+
+        private ObservableCollection<MenuGroupModel> _Nodes = new ObservableCollection<MenuGroupModel>();
+
+        public ObservableCollection<MenuGroupModel> Nodes
+        {
+            get { return _Nodes; }
+            set
+            {
+                _Nodes = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
